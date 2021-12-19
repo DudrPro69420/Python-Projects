0d7d462dd6380c065738c68af8f26f7fc699ae69
DudrPro69420 <96123344+DudrPro69420@users.noreply.github.com>
2021-12-19 10:55:13 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,26 +1,38 @@
 import random 
 from words import wordList
 
-#it requires words.py file.
+guessed = False
+fakeWord = []
+tries = 6
+wordLocations = []
 
+#this will return a word from word.py
 def getWord():
     word = random.choice(wordList)
+    if len(word)>8:
+        getWord()
+
     return word.upper()
 
 
 def play(word):
-    fakeWord = []
+    global guessed
+    global fakeWord
+    global tries
+    global wordLocations
+
     for i in range(len(word)):
         fakeWord.append("_")
 
-    guessed = False
     gussedLetters = [word[0], word[-1]]
-    tries = 6
+
     print("Starting hangman!")
     print(displayHangman(tries))
     print("Guess the word!")
+
     fakeWord[0] = word[0]
     fakeWord[-1] = word[-1]
+    
     for i in range(len(fakeWord)):
         print(fakeWord[i], end = "")
     print("")
@@ -28,10 +40,11 @@ def play(word):
     while not guessed and tries > 0:
         guess = input("Guess a letter: ").upper()
         howMany = word.count(guess)
+
         if len(guess)>1:
             print("please enter only 1 letter.")
 
-        elif guess in gussedLetters and howMany > 1:
+        elif guess in gussedLetters and howMany <= 1:
             print("You've already used that letter.")
             
         elif guess not in word:
@@ -44,19 +57,44 @@ def play(word):
             print("You've correctly guessed one letter. Nicr")
             gussedLetters.append(guess)
             tries = tries - 1
+
             print(displayHangman(tries))
+
             whereWord = word.find(guess)
-            fakeWord[whereWord] = guess
-            for i in range(len(fakeWord)):
-                print(fakeWord[i], end = "")
-            print("")
+
+            if whereWord in wordLocations:
+                newWordLocation = findTheWord(whereWord, guess, word)
+                wordLocations.append(newWordLocation)
+                editWordLocation = newWordLocation
+
+            else:
+                editWordLocation = whereWord
+                wordLocations.append(whereWord)
+
+            if editWordLocation == None:
+                print("You've already guessed that letter")
+            else:
+                fakeWord[editWordLocation] = guess
+                for i in range(len(fakeWord)):
+                    print(fakeWord[i], end = "")
+                print("")
 
         if "_" not in fakeWord:
-            guess = True
-            displayResult(guess, word)
+            guessed = True
+            displayResult(guessed, word)
 
         if tries == 0:
             displayResult(False, word)
+
+def findTheWord(whereWord, guess, word):
+    global wordLocations
+
+    whereNextWord = word.find(guess, whereWord+1)
+
+    if whereNextWord in wordLocations:
+        findTheWord(whereNextWord, guess, word)
+    
+    return whereNextWord
 
 
 def displayResult(winOrLose, word):
